a7e8777418db082289f505c9db294027d7761a7e
joseph <joseph.2210@hotmail.es>
2024-07-06 18:57:49 -0500

    feat(page 404): create error page

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import Icon from "./../assets/png/Icon.png";
+import { useWebContext } from '../hooks/useWebContext'
+import { useNavigate } from "react-router-dom";
+
 
 function Page404() {
+  const navigate = useNavigate();
+
+  const { isMobile } = useWebContext();
   return (
-    <div>Page404</div>
+    <div className={`errorPage-container ${isMobile && 'movil'}`}>
+      <img src={Icon} />
+      <aside>
+        <span className="errorPage-title">
+          404
+        </span>
+        <span className="errorPage-text">La pagina buscada no se encuentra
+        vuelve al </span>
+
+        <button onClick={() => navigate('/dashboard')}>Inicio</button>
+
+      </aside>
+    </div>
   )
 }
 
