bbe39bd645c99a162a444197d4deafb83d25ce57
joseph <joseph.2210@hotmail.es>
2024-07-06 22:05:01 -0500

    feat(dashboard): card component

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import SingleCard from "../components/dashboards/SingleCard";
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <div className="container">
+      <div className="bento-container">
+        <div className="bento-content">
+          <div className="row row-1 ">
+            <div className="col col-1">
+              <SingleCard 
+                title="Mensajes intercambiados"
+                content={1234}
+              />
+              <SingleCard 
+                title="Mensajes fuera de horario de trabajo"
+                content={321}
+              />
+              <SingleCard 
+                title="Pedidos de cotización"
+                content={100}
+              />
+            </div>
+            <div className="col col-2 red"></div>
+          </div>
+          <div className="row row-2 red">
+            <div className="col col-2 red"></div>
+            <div className="col col-1 red"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
